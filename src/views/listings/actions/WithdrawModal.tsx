48d4a18d74cbb17bd43fb5c141767d2dacbff602
Thu Hoài <86884893+thuhoai179@users.noreply.github.com>
2021-12-27 17:25:05 +0700

    add formik form and tokenAmount to withdraw token

--- src/views/listings/actions/WithdrawModal.tsx
+++ src/views/listings/actions/WithdrawModal.tsx
@@ -1,8 +1,34 @@
-import { CButton, CCol, CModal, CModalBody, CModalFooter, CModalHeader, CModalTitle } from '@coreui/react';
-import React, { useEffect } from 'react';
+import {
+  CButton,
+  CCol,
+  CForm,
+  CFormGroup,
+  CInput,
+  CInputGroup,
+  CInputGroupAppend,
+  CInvalidFeedback,
+  CLabel,
+  CModal,
+  CModalBody,
+  CModalFooter,
+  CModalHeader,
+  CModalTitle,
+  CRow,
+} from '@coreui/react';
+import dayjs from 'dayjs';
+import { BigNumber } from 'ethers';
+import { Formik } from 'formik';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import * as Yup from 'yup';
 import { LISTING_INSTANCE } from '../../../shared/blockchain-helpers';
-import { estimateWithdrawAmount, insertCommas } from '../../../shared/casual-helpers';
+import {
+  convertDecimalToBn,
+  estimateWithdrawAmount,
+  insertCommas,
+  unInsertCommas,
+} from '../../../shared/casual-helpers';
+import { ToastError } from '../../../shared/components/Toast';
 import { EventType } from '../../../shared/enumeration/eventType';
 import { RootState } from '../../../shared/reducers';
 import { selectEntityById } from '../../assets/assets.reducer';
@@ -16,19 +42,30 @@ interface IWithdrawModal {
   setVisibility: (visible: boolean) => void;
 }
 
+interface IIntialValues {
+  tokenAmount: number;
+}
+const currentUnix = dayjs().unix();
+
 const WithdrawModal = (props: IWithdrawModal) => {
   const { isVisible, setVisibility, listingId } = props;
   const dispatch = useDispatch();
 
   const listing = useSelector(selectEntityById(listingId));
-  
   const { signer } = useSelector((state: RootState) => state.wallet);
-
   const { submitted } = useSelector((state: RootState) => state.transactions);
 
   const closeModal = () => (): void => setVisibility(false);
 
-  const withdrawValues = () => {
+  const initialValues: IIntialValues = {
+    tokenAmount: 0,
+  };
+
+  const validationSchema = Yup.object().shape({
+    tokenAmount: Yup.number().typeError('Số lượng token không hợp lệ').required('Vui lòng nhập số token muốn nạp'),
+  });
+
+  const handleRawFormValues = (input: IIntialValues): IProceedTxBody => {
     if (!listing?.address) {
       throw Error('Error getting listing address');
     }
@@ -44,15 +81,16 @@ const WithdrawModal = (props: IWithdrawModal) => {
       listingId,
       contract: instance,
       type: EventType.WITHDRAW,
-      args: { ...baseSetterArgs },
+      args: { ...baseSetterArgs, _amount: convertDecimalToBn(input.tokenAmount.toString()) },
     };
 
     return output;
   };
 
-  const onWithdrawConfirm = () => (): void => {
-    dispatch(fetching());
-    dispatch(proceedTransaction(withdrawValues()));
+  const [maxTokenWithdraw, setMaxTokenWithdraw] = useState<number | string>(0);
+
+  const getMaximumWithdrawAmount = () => {
+    setMaxTokenWithdraw(withdrawTokenAmount);
   };
 
   useEffect(() => {
@@ -62,36 +100,126 @@ const WithdrawModal = (props: IWithdrawModal) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [submitted]);
 
+  const [curretUnix, setCurrntUnix] = useState<number>(currentUnix);
 
+  useEffect(() => {
+    setCurrntUnix(dayjs().unix());
+  }, [isVisible]);
+
+  const withdrawTokenAmount =
+    listing?.dailyPayment && listing.ownership
+      ? insertCommas(estimateWithdrawAmount(listing.dailyPayment, listing.ownership.sub(120), curretUnix))
+      : '_';
+
+  const initialSeconds = 60;
+  const [seconds, setSeconds] = useState<number>(initialSeconds);
+
+  // useEffect(() => {
+  //   let myInterval = setInterval(() => {
+  //     if (seconds > 0) {
+  //       setSeconds(seconds - 1);
+  //     }
+  //     // if (seconds === 0) {
+  //     //   setIsSubmitDisable(true);
+  //     // }
+  //   }, 1000);
+  //   return () => {
+  //     clearInterval(myInterval);
+  //   };
+  // });
+  
   return (
     <CModal show={isVisible} onClose={closeModal()} centered className="border-radius-modal">
       <CModalHeader className="justify-content-center">
         <CModalTitle className="modal-title-style">Rút ANFT</CModalTitle>
       </CModalHeader>
-      <CModalBody>
-        {listing?.dailyPayment && listing?.ownership ? (
-          <p>
-            Bạn chắc chắn muốn rút{' '}
-            <span className="text-primary">
-              {insertCommas(estimateWithdrawAmount(listing.dailyPayment, listing.ownership))} ANFT
-            </span>
-          </p>
-        ) : (
-          ''
+      <Formik
+        enableReinitialize
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={(rawValues) => {
+          try {
+            const value = handleRawFormValues(rawValues);
+            dispatch(fetching());
+            dispatch(proceedTransaction(value));
+          } catch (error) {
+            console.log(`Error submitting form ${error}`);
+            ToastError(`Error submitting form ${error}`);
+          }
+        }}
+      >
+        {({ values, errors, touched, handleChange, handleSubmit, handleBlur, setFieldValue }) => (
+          <CForm onSubmit={handleSubmit}>
+            <CModalBody>
+              <CRow>
+                <CCol xs={12}>
+                  <CFormGroup row>
+                    <CCol xs={8}>
+                      <CLabel className="withdraw-token-title">Số ANFT Tối đa bạn rút</CLabel>
+                    </CCol>
+                    <CCol xs={4}>
+                      <p className="text-primary text-right">{withdrawTokenAmount}</p>
+                    </CCol>
+                  </CFormGroup>
+                  <CFormGroup row>
+                    <CCol xs={12}>
+                      <CLabel className="withdraw-token-title">Số ANFT muốn rút</CLabel>
+                    </CCol>
+                    <CCol xs={12}>
+                      <CInputGroup>
+                        <CInput
+                          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                            setFieldValue(`tokenAmount`, unInsertCommas(e.target.value));
+                          }}
+                          id="tokenAmount"
+                          autoComplete="off"
+                          name="tokenAmount"
+                          value={values.tokenAmount || maxTokenWithdraw || ''}
+                          onBlur={handleBlur}
+                          className="btn-radius-50"
+                          type="number"
+                        />
+                        <CInputGroupAppend>
+                          <CButton
+                            color="primary"
+                            className="btn-register-level"
+                            onClick={() => getMaximumWithdrawAmount()}
+                          >
+                            MAX
+                          </CButton>
+                        </CInputGroupAppend>
+                      </CInputGroup>
+
+                      <CInvalidFeedback className={!!errors.tokenAmount && touched.tokenAmount ? 'd-block' : 'd-none'}>
+                        {errors.tokenAmount}
+                      </CInvalidFeedback>
+                    </CCol>
+                  </CFormGroup>
+                </CCol>
+              </CRow>
+            </CModalBody>
+            <CModalFooter className="justify-content-between">
+              <CCol>
+                <CButton
+                  className="px-2 w-100 btn-font-style btn btn-outline-primary btn-radius-50"
+                  onClick={closeModal()}
+                >
+                  HỦY
+                </CButton>
+              </CCol>
+              <CCol>
+                {seconds !== 0 ? (
+                  <CButton className="px-2 w-100 btn btn-primary btn-font-style btn-radius-50" type="submit">
+                    XÁC NHẬN {`(${seconds}s)`}
+                  </CButton>
+                ) : (
+                  <CButton className="px-2 w-100 btn btn-primary btn-font-style btn-radius-50">REFRESH</CButton>
+                )}
+              </CCol>
+            </CModalFooter>
+          </CForm>
         )}
-      </CModalBody>
-      <CModalFooter className="justify-content-between">
-        <CCol>
-          <CButton className="px-2 w-100 btn-font-style btn btn-outline-primary btn-radius-50" onClick={closeModal()}>
-            HỦY
-          </CButton>
-        </CCol>
-        <CCol>
-          <CButton className="px-2 w-100 btn btn-primary btn-font-style btn-radius-50" onClick={onWithdrawConfirm()}>
-            XÁC NHẬN
-          </CButton>
-        </CCol>
-      </CModalFooter>
+      </Formik>
     </CModal>
   );
 };
