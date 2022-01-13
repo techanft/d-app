0f5f893c5102ac74ea04f4c577841faae9728bde
Thang Le <jakethecat21@gmail.com>
2022-01-13 10:22:02 +0700

    improvements on chainId checking

--- src/containers/TheContent.tsx
+++ src/containers/TheContent.tsx
@@ -1,14 +1,12 @@
 import { CContainer, CFade } from '@coreui/react';
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { _window } from '../config/constants';
 // routes config
 import routes from '../routes';
-import { providerNetworkChecking } from '../shared/blockchain-helpers';
 import { RootState } from '../shared/reducers';
 import { getSigner } from '../views/wallet/wallet.api';
-import { setProviderError } from '../views/wallet/wallet.reducer';
 
 const loading = (
   <div className="pt-3 text-center">
@@ -19,14 +17,7 @@ const loading = (
 const TheContent = () => {
   const dispatch = useDispatch();
   const { provider } = useSelector((state: RootState) => state.wallet);
-
-  useEffect(() => {
-    if (!provider) return;
-    const error = providerNetworkChecking(provider);
-    dispatch(setProviderError(error));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [provider]);
-
+ 
   _window.ethereum.on('accountsChanged', () => {
     if (provider) {
       dispatch(getSigner(provider));
